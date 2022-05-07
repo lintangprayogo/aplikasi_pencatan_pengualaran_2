13af4c509e7f14d6ae977d03e0927912eae08b16
lintang <lintang@cpssoft.com>
2022-05-07 22:20:10 +0700

    orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:aplikasi_pencatan_pengualaran/widget/chart/chart_widget.dart';
-import 'package:aplikasi_pencatan_pengualaran/widget/custom_card/expense_card_widget.dart';
 import 'package:aplikasi_pencatan_pengualaran/widget/input/input_expense_widget.dart';
+import 'package:aplikasi_pencatan_pengualaran/widget/main/main_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:aplikasi_pencatan_pengualaran/model/expense.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 
 void main() {
+  //setting to orientation
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(const MyApp());
 }
 
@@ -23,13 +28,6 @@ class _MyAppState extends State<MyApp> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
   final List<Expense> _expenses = [];
-
-  List<Expense> get recentExpense {
-    return _expenses
-        .where((element) =>
-            element.date.isAfter(DateTime.now().subtract(Duration(days: 7))))
-        .toList();
-  }
 
   void _saveExpense(String title, int nominal, DateTime dateTime) {
     Expense newExpense = Expense(
@@ -60,8 +58,28 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext ctx) {
     initializeDateFormatting(); //Initialisasi Tanggal
+
+    AppBar appBar = AppBar(
+      title: Text(
+        "Pencatatan Pengeluaran",
+        // style: TextStyle(fontFamily: "OpenSans"),
+      ),
+      actions: [
+        IconButton(
+            onPressed: () {
+              BuildContext? curentContext = scaffoldKey.currentContext;
+              if (curentContext != null) {
+                _showExpenseForm(curentContext);
+              }
+            },
+            icon: Icon(
+              Icons.add,
+              color: Colors.white,
+            ))
+      ],
+    );
 
     return MaterialApp(
       theme: ThemeData(
@@ -73,88 +91,13 @@ class _MyAppState extends State<MyApp> {
                       TextStyle(fontFamily: "OpenSans", fontSize: 30)))),
       home: Scaffold(
         key: scaffoldKey,
-        appBar: AppBar(
-          title: Text(
-            "Pencatatan Pengeluaran",
-            // style: TextStyle(fontFamily: "OpenSans"),
-          ),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  BuildContext? curentContext = scaffoldKey.currentContext;
-                  if (curentContext != null) {
-                    _showExpenseForm(curentContext);
-                  }
-                },
-                icon: Icon(
-                  Icons.add,
-                  color: Colors.white,
-                ))
-          ],
-        ),
+        appBar: appBar,
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Chart(recentExpense: recentExpense),
-              SizedBox(
-                height: 300,
-                child: _expenses.isEmpty
-                    ? Column(
-                        children: [
-                          Text("No expenses have been added yet"),
-                          SizedBox(
-                            height: 200,
-                            child: Image.asset(
-                              "assets/images/waiting.png",
-                              fit: BoxFit.cover,
-                            ),
-                          )
-                        ],
-                      )
-                    : ListView.builder(
-                        itemCount: _expenses.length,
-                        itemBuilder: (context, index) {
-                          // return ExpenseCardWidget(expense: _expenses[index]);
-                          return Card(
-                            margin: EdgeInsets.symmetric(
-                                horizontal: 8, vertical: 5),
-                            child: ListTile(
-                              //widget that start from Begining
-                              leading: CircleAvatar(
-                                radius: 30,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(6.0),
-                                  child: FittedBox(
-                                    child: Text(NumberFormat.compactCurrency(
-                                            locale: "id", decimalDigits: 0)
-                                        .format(_expenses[index].nominal)),
-                                  ),
-                                ),
-                              ),
-                              title: Text(
-                                _expenses[index].title,
-                                style: Theme.of(context).textTheme.titleLarge,
-                              ),
-                              subtitle: Text(
-                                DateFormat("dd/MM/yyyy")
-                                    .format(_expenses[index].date),
-                                style: Theme.of(context).textTheme.subtitle1,
-                              ),
-                              trailing: IconButton(
-                                onPressed: () {
-                                  deleteItem(index);
-                                },
-                                icon: Icon(Icons.delete),
-                                color: Colors.red,
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-              ),
-            ],
-          ),
-        ),
+            child: MainWidget(
+          expenses: _expenses,
+          deleteItem: deleteItem,
+          appBarHeight: appBar.preferredSize.height,
+        )),
         floatingActionButton: FloatingActionButton(
             onPressed: () {
               BuildContext? curentContext = scaffoldKey.currentContext;
--- lib/widget/chart/chart_bar_widget.dart
+++ lib/widget/chart/chart_bar_widget.dart
@@ -15,44 +15,48 @@ class ChartBarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 20,
-          child: FittedBox(
-            child: Text(NumberFormat.compactSimpleCurrency(
-                    locale: 'id', decimalDigits: 0)
-                .format(expenseAmount)),
-          ),
-        ),
-        const SizedBox(
-          height: 4,
-        ),
-        SizedBox(
-          height: 60,
-          width: 10,
-          child: Stack(children: [
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: Colors.grey, width: 1.0),
-                  color: const Color.fromRGBO(220, 229, 200, 1)),
+    return LayoutBuilder(builder: (ctx, constraints) {
+      return Column(
+        children: [
+          SizedBox(
+            height: constraints.maxHeight * 0.15,
+            child: FittedBox(
+              child: Text(NumberFormat.compactSimpleCurrency(
+                      locale: 'id', decimalDigits: 0)
+                  .format(expenseAmount)),
             ),
-            FractionallySizedBox(
-              heightFactor: expensePrecentange,
-              child: Container(
+          ),
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          SizedBox(
+            height: constraints.maxHeight * 0.6,
+            width: 10,
+            child: Stack(children: [
+              Container(
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
-                    color: Theme.of(context).primaryColor),
+                    border: Border.all(color: Colors.grey, width: 1.0),
+                    color: const Color.fromRGBO(220, 229, 200, 1)),
               ),
-            )
-          ]),
-        ),
-        const SizedBox(
-          height: 4,
-        ),
-        Text(day)
-      ],
-    );
+              FractionallySizedBox(
+                heightFactor: expensePrecentange,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Theme.of(context).primaryColor),
+                ),
+              )
+            ]),
+          ),
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          SizedBox(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(child: Text(day)))
+        ],
+      );
+    });
   }
 }
